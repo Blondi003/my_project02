81ba792851a15b9cc67cb23bbd68a9349356c1c5
Blondi003 <59411510+Blondi003@users.noreply.github.com>
2020-06-17 13:28:14 +0200

    my first repository

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import CounterExample from './CounterExample';
+
+function App() {
+  return <div>{<CounterExample />}</div>;
+}
+
+export default App;
